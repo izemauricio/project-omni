1a701c8ecb05afcd9f776c4616d17fcb4e39b6f8
Mauricio Ize <mauriciodcjz@gmail.com>
2020-03-30 03:29:54 -0300

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const app = express();
+app.use(cors({
+    origin:'localhost'
+}));
 app.use(express.json()); // allow express to understand body request as json
 app.use(routes);
 app.listen(3333);
